b283cb782da0c1663e86d371c9a08b3426af1f7b
LazyTechwork <lazytechwork@gmail.com>
2021-04-07 19:24:29 +0300

    Much more layout fixes

--- src/App.jsx
+++ src/App.jsx
@@ -77,74 +77,76 @@ const App = () => {
                                 </PanelHeaderContent>
                             </PanelHeader>
                             <Group>
-                                <FormLayout>
-                                    <FormItem top="Оценки">
-                                        <Input onChange={onAllGradesChange} inputmode="numeric"
-                                               value={grades.join(" ")}
-                                               placeholder="Введите здесь оценки"/>
-                                    </FormItem>
-                                    <FormLayoutGroup mode="horizontal">
-                                        <FormItem top="Колы">
-                                            <Input type="number" value={grades.filter(x => x === 1).length}
-                                                   onChange={(e) => onSeparateGradesChange(e.target.value, 1)}
-                                                   min={0}
-                                                   max={500}/>
+                                <Div>
+                                    <FormLayout>
+                                        <FormItem top="Оценки">
+                                            <Input onChange={onAllGradesChange} inputmode="numeric"
+                                                   value={grades.join(" ")}
+                                                   placeholder="Введите здесь оценки"/>
                                         </FormItem>
-                                        <FormItem top="Двойки">
-                                            <Input type="number" value={grades.filter(x => x === 2).length}
-                                                   onChange={(e) => onSeparateGradesChange(e.target.value, 2)}
-                                                   min={0}
-                                                   max={500}/>
-                                        </FormItem>
-                                    </FormLayoutGroup>
-                                    <FormLayoutGroup mode="horizontal">
-                                        <FormItem top="Тройки">
-                                            <Input type="number" value={grades.filter(x => x === 3).length}
-                                                   onChange={(e) => onSeparateGradesChange(e.target.value, 3)}
-                                                   min={0}
-                                                   max={500}/>
-                                        </FormItem>
-                                        <FormItem top="Четвёрки">
-                                            <Input type="number" value={grades.filter(x => x === 4).length}
-                                                   onChange={(e) => onSeparateGradesChange(e.target.value, 4)}
-                                                   min={0}
-                                                   max={500}/>
-                                        </FormItem>
-                                        <FormItem top="Пятёрки">
-                                            <Input type="number" value={grades.filter(x => x === 5).length}
-                                                   onChange={(e) => onSeparateGradesChange(e.target.value, 5)}
-                                                   min={0}
-                                                   max={500}/>
-                                        </FormItem>
-                                    </FormLayoutGroup>
+                                        <FormLayoutGroup mode="horizontal">
+                                            <FormItem top="Колы">
+                                                <Input type="number" value={grades.filter(x => x === 1).length}
+                                                       onChange={(e) => onSeparateGradesChange(e.target.value, 1)}
+                                                       min={0}
+                                                       max={500}/>
+                                            </FormItem>
+                                            <FormItem top="Двойки">
+                                                <Input type="number" value={grades.filter(x => x === 2).length}
+                                                       onChange={(e) => onSeparateGradesChange(e.target.value, 2)}
+                                                       min={0}
+                                                       max={500}/>
+                                            </FormItem>
+                                        </FormLayoutGroup>
+                                        <FormLayoutGroup mode="horizontal">
+                                            <FormItem top="Тройки">
+                                                <Input type="number" value={grades.filter(x => x === 3).length}
+                                                       onChange={(e) => onSeparateGradesChange(e.target.value, 3)}
+                                                       min={0}
+                                                       max={500}/>
+                                            </FormItem>
+                                            <FormItem top="Четвёрки">
+                                                <Input type="number" value={grades.filter(x => x === 4).length}
+                                                       onChange={(e) => onSeparateGradesChange(e.target.value, 4)}
+                                                       min={0}
+                                                       max={500}/>
+                                            </FormItem>
+                                            <FormItem top="Пятёрки">
+                                                <Input type="number" value={grades.filter(x => x === 5).length}
+                                                       onChange={(e) => onSeparateGradesChange(e.target.value, 5)}
+                                                       min={0}
+                                                       max={500}/>
+                                            </FormItem>
+                                        </FormLayoutGroup>
 
-                                    <FormLayoutGroup mode="horizontal">
-                                        <FormItem top="Рассчитанный балл">
-                                            <Input readOnly
-                                                   value={toGrade(grades.reduce((p, c) => p + c, 0) / grades.length)}/>
-                                        </FormItem>
-                                        <FormItem top="Требуемый балл">
-                                            <Input type="number" min={1} max={5} step={0.1} inputmode="decimal"
-                                                   value={required}
-                                                   onBlur={(e) => {
-                                                       e.target.value = toGrade(parseFloat(e.target.value))
-                                                       setRequired(parseFloat(e.target.value))
-                                                   }}
-                                                   onChange={(e) => setRequired(parseFloat(e.target.value))}
-                                                   placeholder="Введите здесь нужный балл"/>
-                                        </FormItem>
-                                    </FormLayoutGroup>
-                                    <FormLayoutGroup mode="horizontal">
-                                        <FormItem top="Необходимо четвёрок">
-                                            <Input readOnly value={requiredGradesCount(4)}/>
-                                        </FormItem>
-                                        <FormItem top="Необходимо пятёрок">
-                                            <Input readOnly value={requiredGradesCount(5)}/>
-                                        </FormItem>
-                                    </FormLayoutGroup>
-                                </FormLayout>
-                                <Separator wide style={{marginTop: 16, marginBottom: 8}}/>
-                                <Developers/>
+                                        <FormLayoutGroup mode="horizontal">
+                                            <FormItem top="Рассчитанный балл">
+                                                <Input readOnly
+                                                       value={toGrade(grades.reduce((p, c) => p + c, 0) / grades.length)}/>
+                                            </FormItem>
+                                            <FormItem top="Требуемый балл">
+                                                <Input type="number" min={1} max={5} step={0.1} inputmode="decimal"
+                                                       value={required}
+                                                       onBlur={(e) => {
+                                                           e.target.value = toGrade(parseFloat(e.target.value))
+                                                           setRequired(parseFloat(e.target.value))
+                                                       }}
+                                                       onChange={(e) => setRequired(parseFloat(e.target.value))}
+                                                       placeholder="Введите здесь нужный балл"/>
+                                            </FormItem>
+                                        </FormLayoutGroup>
+                                        <FormLayoutGroup mode="horizontal">
+                                            <FormItem top="Необходимо четвёрок">
+                                                <Input readOnly value={requiredGradesCount(4)}/>
+                                            </FormItem>
+                                            <FormItem top="Необходимо пятёрок">
+                                                <Input readOnly value={requiredGradesCount(5)}/>
+                                            </FormItem>
+                                        </FormLayoutGroup>
+                                    </FormLayout>
+                                    <Separator wide style={{marginTop: 16, marginBottom: 8}}/>
+                                    <Developers/>
+                                </Div>
                                 {advertisement !== null && <FixedLayout vertical="bottom">
                                     <PromoBanner bannerData={advertisement} isCloseButtonHidden
                                                  onClose={() => setAdvertisement(null)}/>
@@ -155,8 +157,7 @@ const App = () => {
                 </AppRoot>
             </AdaptivityProvider>
         </ConfigProvider>
-    )
-        ;
+    );
 }
 
 export default App;
