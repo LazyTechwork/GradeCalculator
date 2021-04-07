a856e372d8313adcab84c01dad52098798029d99
LazyTechwork <lazytechwork@gmail.com>
2021-04-07 17:47:26 +0300

    Added information about developers

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,29 @@
 import React, {useEffect, useState} from 'react';
 import bridge from '@vkontakte/vk-bridge';
 import {
-    Div,
-    View,
     AdaptivityProvider,
+    Appearance,
     AppRoot,
+    Avatar,
+    Banner,
+    Button,
+    ConfigProvider,
+    Div,
+    FormItem,
+    FormLayout,
+    FormLayoutGroup,
+    Header,
+    Input,
     Panel,
     PanelHeader,
-    FormLayout,
-    FormItem,
-    Input,
-    FormLayoutGroup, PanelHeaderContent, Avatar, Banner, Button, ConfigProvider, Appearance, Scheme
+    PanelHeaderContent,
+    Scheme,
+    Separator,
+    View
 } from '@vkontakte/vkui';
 import '@vkontakte/vkui/dist/vkui.css';
 import ServiceIcon from './img/icon.svg';
-import {Icon24FavoriteOutline} from '@vkontakte/icons';
+import {Icon24FavoriteOutline, Icon24MessageOutline} from '@vkontakte/icons';
 
 const App = () => {
     const [appearance, setAppearance] = useState(Appearance.LIGHT);
@@ -104,12 +113,21 @@ const App = () => {
                                         </FormItem>
                                     </FormLayoutGroup>
                                 </FormLayout>
+                                <Separator wide style={{marginTop: 16, marginBottom: 8}}/>
+                                <Header>Разработчики</Header>
                                 <Banner
                                     before={<Avatar size={96} mode="image"
                                                     src="https://sun1-95.userapi.com/s/v1/ig2/PdhJ0tQePfaEed-XUUpfysiQbL69Qd3OP97eyenSj_QC7n5HcOB6_qjOZVr4KppPVagSFabgCeQDKNe5XwQ7IrvY.jpg?size=200x0&quality=96&crop=0,0,2160,2160&ava=1"/>}
                                     header="ФРИИС"
                                     subheader="Фонд развития информационных и интеллектуальных систем"
                                     actions={<Button before={<Icon24FavoriteOutline/>}>Подписаться</Button>}
+                                />
+                                <Banner
+                                    before={<Avatar size={96} mode="image"
+                                                    src="https://sun1-17.userapi.com/s/v1/ig2/k8gq8e-VsnNptU4cZ2m2xLFVQd1QRtl1B9P_duk2G0W0hVp5yGbTB7Goq7-Pe3OG4-3SYNh9kqQBWsYZ3Dp1DKHP.jpg?size=200x0&quality=96&crop=392,66,1136,1136&ava=1"/>}
+                                    header="Петров Иван"
+                                    subheader="Разработчик"
+                                    actions={<Button before={<Icon24MessageOutline/>}>Написать</Button>}
                                 />
                             </Div>
                         </Panel>
